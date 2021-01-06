9bfe209f62c100bcec1842cf003e65d17b934eef
sunaneko <atmfossamaguna@yahoo.co.jp>
2021-01-06 21:42:05 +0900

    平衡三進法での表記を追加した
    答えを平衡三進法で表記した行を追加した。
    
    平衡三進法での表記の求め方は自信がない。正の整数についてはあるサイトを参考にしたが、負の整数についてはそれをもとに適当に変えただけなのであっているか分からないし、どれだけ正しく動くかも検証していない。

--- sanshin.js
+++ sanshin.js
@@ -49,7 +49,7 @@ function sanshin(pattern) {
   }
 
   let ans = [];
-  for (let i = 0; i < pattern.length + 4; i++) {
+  for (let i = 0; i < pattern.length + 5; i++) {
     ans[i] = [];
   }
 
@@ -73,6 +73,9 @@ function sanshin(pattern) {
 
     // 答えの3進数の表記をいれる
     ans[pattern.length + 3][i] = ans[pattern.length + 2][i].toString(3);
+
+    // 答えの平衡三進法の表記をいれる
+    ans[pattern.length + 4][i] = balTer(ans[pattern.length + 2][i]);
   }
 
   return ans;
@@ -99,6 +102,41 @@ function makeTable(data, tableId) {
   }
   // 指定したdiv要素に表を加える
   document.getElementById(tableId).appendChild(table);
+}
+
+//10進数を平衡三進法の表記へ変換する
+function balTer(value) {
+  let output = "";
+  let rem;
+
+  if (value == 0) {
+    //0の場合
+    output = "0";
+  } else if (value > 0) {
+    // 0以外の正の整数の場合
+    while (value > 0) {
+      rem = value % 3;
+      value = Math.floor(value / 3);
+      if (rem == 2) {
+        rem = -1;
+        value++;
+      }
+      output = (rem == 0 ? "0" : rem == 1 ? "1" : "T") + output;
+    }
+  } else if (value < 0) {
+    // 0以外の負の整数の場合(正しく実装できているか自信がないし確かめてもいない)
+    while (value < 0) {
+      rem = value % 3;
+      value = Math.trunc(value / 3);
+      if (rem == -2) {
+        rem = 1;
+        value--;
+      }
+      output = (rem == 0 ? "0" : rem == 1 ? "1" : "T") + output;
+    }
+  }
+
+  return output;
 }
 
 $("#form").submit(function () {
